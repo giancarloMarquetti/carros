adcc32436a16e5e281d57fc770c89fe631105f59
Giancarlo <gianmarquetti@gmail.com>
2025-07-15 20:43:02 -0300

    config allowed_hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yhm45#8b(__#jtaxl+zee2=ueo27c@(9ijts1hx-ln=%cu&$%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
